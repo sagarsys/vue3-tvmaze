67aabf92c304ac15e697a1e1d76d8cb31ded560b
sagar <sagar.sawuck@gmail.com>
2026-05-04 00:02:08 +0200

    add strict mode and path alias

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import viteLogo from '../assets/vite.svg'
-import heroImg from '../assets/hero.png'
-import vueLogo from '../assets/vue.svg'
+import viteLogo from '@/assets/vite.svg'
+import heroImg from '@/assets/hero.png'
+import vueLogo from '@/assets/vue.svg'
 
 const count = ref(0)
 </script>
@@ -15,7 +15,7 @@ const count = ref(0)
       <img :src="viteLogo" class="vite" alt="Vite logo" />
     </div>
     <div>
-      <h1>Get started</h1>
+      <h1>Get started!</h1>
       <p>Edit <code>src/App.vue</code> and save to test <code>HMR</code></p>
     </div>
     <button type="button" class="counter" @click="count++">
