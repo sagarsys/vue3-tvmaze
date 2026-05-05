a2562f9f0b082c6835c7b7de29800925aa705b65
sagar <sagar.sawuck@gmail.com>
2026-05-06 01:57:04 +0200

    fix issue with button styling on mobile

--- src/components/show-detail/ShowDetailViewLink.vue
+++ src/components/show-detail/ShowDetailViewLink.vue
@@ -10,14 +10,16 @@ defineProps<Props>();
 </script>
 
 <template>
-  <a
-    :href="url"
-    target="_blank"
-    rel="noopener noreferrer"
-    :aria-label="`View ${name} on TVMaze (opens in new tab)`"
-    class="bg-primary px-4 py-2 flex w-50 rounded-md text-base font-medium text-background transition-colors hover:bg-primary/90 focus-ring-accent justify-self-center md:justify-self-start"
-  >
-    <CirclePlay class="mr-2 h-5 w-5" />
-    View on TVMaze
-  </a>
+  <div class="flex justify-center md:justify-start my-4">
+    <a
+      :href="url"
+      target="_blank"
+      rel="noopener noreferrer"
+      :aria-label="`View ${name} on TVMaze (opens in new tab)`"
+      class="bg-primary px-4 py-2 flex w-50 rounded-md text-base font-medium text-background transition-colors hover:bg-primary/90 focus-ring-accent"
+    >
+      <CirclePlay class="mr-2 h-5 w-5" />
+      View on TVMaze
+    </a>
+  </div>
 </template>
